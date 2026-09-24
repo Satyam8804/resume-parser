1ba52a1b637b720988c7355eed6c095c7dfbd9fd
Satyam Kumar <satyam8804378323@gmail.com>
2026-09-25 00:01:56 +0530

    fix issue

--- backend/app/utils/evaluate_score.py
+++ backend/app/utils/evaluate_score.py
@@ -6,7 +6,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
-model = "llama-3.3-70b-versatile"
+model = "openai/gpt-oss-120b"
+
 
 
 def build_resume_summary(resume: dict) -> str:
